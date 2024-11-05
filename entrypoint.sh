b25a5a6eac878d5e9e442fac6f7b78f9af3b2388
doohochang <doohochang@gmail.com>
2024-11-05 16:57:21 +0900

    chore: add comments for issue reporting

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,9 @@
 java -jar /run.jar $1 $2 >> $GITHUB_STEP_SUMMARY
 STATUS=$?
 
+echo "---" >> $GITHUB_STEP_SUMMARY
+echo "If you notice any incorrect errors or have suggestions for improvement, please [create an issue](https://github.com/portone-io/openapi-inspector/issues/new) to let us know!" >> $GITHUB_STEP_SUMMARY
+
 if [ $STATUS != 0 ]
 then
     echo "::error::OpenAPI Validation Failed!"
